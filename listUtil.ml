c82ac631f388758acf8276b923df5a73755cebd6
Menelaos Perdikeas <mperdikeas@turin.(none)>
2012-03-16 17:20:43 +0000

    fxd bug in listUtil

--- listUtil.ml
+++ listUtil.ml
@@ -1,15 +1,6 @@
 exception Exception of string
 exception Bug
 
-let split l n = 
-if n <= 0 then raise (Invalid_argument (Printf.sprintf "split n=%d; can't do" n))
-else
-  let rec split0 l i accum = match l, i with
-    | _, 0 -> accum
-    | [], _    -> split0 [] (i-1) ([]::accum)
-    | h::tl, _ -> split0 tl (i-1) ((every_nth l n 0)::accum)
-  in List.rev (split0 l n [])
-
 let tl_nth l n = 
   let rec tl_nth0 n accum = match n, accum with
     | 0, _     -> accum
@@ -28,4 +19,18 @@ let every_nth l n offset = (* start on zero-indexed offset and pick every nth el
         else           every_nth0 tl (m+1) accum
     | [] -> accum
   in List.rev (every_nth0 (tl_nth l offset) n [])
+
+
+
+let split l n = 
+if n <= 0 then raise (Invalid_argument (Printf.sprintf "split n=%d; can't do" n))
+else
+  let rec split0 l i accum = match l, i with
+    | _, 0 -> accum
+    | [], _    -> split0 [] (i-1) ([]::accum)
+    | h::tl, _ -> split0 tl (i-1) ((every_nth l n 0)::accum)
+  in List.rev (split0 l n [])
+
+
+
 
